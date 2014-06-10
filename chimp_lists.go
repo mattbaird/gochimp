490cbc7216dc72ed29bbacf723aa527a3983489d
Zac Gross <zacgross@gmail.com>
2014-06-10 12:29:41 -0400

    implemented lists/list method. Spec: http://apidocs.mailchimp.com/api/2.0/lists/list.php

--- chimp_lists.go
+++ chimp_lists.go
@@ -15,6 +15,7 @@ package gochimp
 const (
 	lists_subscribe_endpoint     string = "/lists/subscribe.json"
 	lists_unsubscribe_endpoint   string = "/lists/unsubscribe.json"
+	lists_list_endpoint          string = "/lists/list.json"
 	lists_update_member_endpoint string = "/lists/update-member.json"
 )
 
@@ -28,6 +29,13 @@ func (a *ChimpAPI) ListsSubscribe(req ListsSubscribe) (Email, error) {
 func (a *ChimpAPI) ListsUnsubscribe(req ListsUnsubscribe) error {
 	req.ApiKey = a.Key
 	return parseChimpJson(a, lists_unsubscribe_endpoint, req, nil)
+}
+
+func (a *ChimpAPI) ListsList(req ListsList) (ListsListResponse, error) {
+	req.ApiKey = a.Key
+	var response ListsListResponse
+	err := parseChimpJson(a, lists_unsubscribe_endpoint, req, &response)
+	return response, err
 }
 
 func (a *ChimpAPI) UpdateMember(req UpdateMember) error {
@@ -54,6 +62,76 @@ type ListsSubscribe struct {
 	UpdateExisting   bool                   `json:"update_existing"`
 	ReplaceInterests bool                   `json:"replace_interests"`
 	SendWelcome      bool                   `json:"send_welcome"`
+}
+
+type ListFilter struct {
+	ListId        string `json:"list_id"`
+	ListName      string `json:"list_name"`
+	FromName      string `json:"from_name"`
+	FromEmail     string `json:"from_email"`
+	FromSubject   string `json:"from_subject"`
+	CreatedBefore string `json:"created_before"`
+	CreatedAfter  string `json:"created_after"`
+	Exact         bool   `json:"exact"`
+}
+
+type ListStat struct {
+	MemberCount               float64 `json:"member_count"`
+	UnsubscribeCount          float64 `json:"unsubscribe_count"`
+	CleanedCount              float64 `json:"cleaned_count"`
+	MemberCountSinceSend      float64 `json:"member_count_since_send"`
+	UnsubscribeCountSinceSend float64 `json:"unsubscribe_count_since_send"`
+	CleanedCountSinceSend     float64 `json:"cleaned_count_since_send"`
+	CampaignCount             float64 `json:"campaign_count"`
+	GroupingCount             float64 `json:"grouping_count"`
+	GroupCount                float64 `json:"group_count"`
+	MergeVarCount             float64 `json:"merge_var_count"`
+	AvgSubRate                float64 `json:"avg_sub_rate"`
+	AvgUnsubRate              float64 `json:"avg_unsub_rate"`
+	TargetSubRate             float64 `json:"target_sub_rate "`
+	OpenRate                  float64 `json:"open_rate "`
+	ClickRate                 float64 `json:"click_rate "`
+}
+
+type ListData struct {
+	Id                string     `json:"id"`
+	WebId             string     `json:"web_id"`
+	Name              string     `json:"name"`
+	DateCreated       string     `json:"date_created"`
+	EmailTypeOption   string     `json:"email_type_option"`
+	UseAwesomeBar     bool       `json:"use_awesomebar"`
+	DefaultFromName   string     `json:"default_from_name"`
+	DefaultFromEmail  string     `json:"default_from_email"`
+	DefaultSubject    string     `json:"default_subject"`
+	DefaultLanguage   string     `json:"default_language"`
+	ListRating        float64    `json:"list_rating"`
+	SubscribeShortUrl string     `json:"subscribe_url_short"`
+	SubscribeLongUrl  string     `json:"subscribe_url_long"`
+	BeamerAddress     string     `json:"beamer_address"`
+	Visibility        string     `json:"visibility"`
+	Stats             []ListStat `json:"stats"`
+	Modules           []string   `json:"modules"`
+}
+
+type ListError struct {
+	Param string `json:"param"`
+	Code  int    `json:"code"`
+	Error string `json:"error"`
+}
+
+type ListsListResponse struct {
+	Total  int         `json:"total"`
+	Data   []ListData  `json:"data"`
+	Errors []ListError `json:"errors"`
+}
+
+type ListsList struct {
+	ApiKey        string     `json:"apikey"`
+	Filters       ListFilter `json:"filters"`
+	Start         int        `json:"start"`
+	Limit         int        `json:"limit"`
+	SortField     string     `json:"sort_field"`
+	SortDirection string     `json:"sort_direction"`
 }
 
 type UpdateMember struct {
