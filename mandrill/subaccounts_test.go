0456045954c1c4a264bae97c266cbd5cbf4278dc
John E Vincent <lusis.org+github.com@gmail.com>
2020-01-27 11:03:07 -0500

    stub out subaccounts tests

---
+++ mandrill/subaccounts_test.go
@@ -0,0 +1,13 @@
+package mandrill
+
+import (
+	"testing"
+)
+
+func TestSubAccountsList(t *testing.T)   {}
+func TestSubAccountsAdd(t *testing.T)    {}
+func TestSubAccountsInfo(t *testing.T)   {}
+func TestSubAccountsUpdate(t *testing.T) {}
+func TestSubAccountsDelete(t *testing.T) {}
+func TestSubAccountsPause(t *testing.T)  {}
+func TestSubAccountsResume(t *testing.T) {}
